97d1cf58d0f11d1954a9f3209e29af00f367feeb
mark <mhuiq@qq.com>
2017-02-05 18:39:19 +0800

    完善功能

--- test/TestMethod.js
+++ test/TestMethod.js
@@ -7,4 +7,15 @@
     var jsonSubject = JSON.parse(jsonStr);
     console.log(jsonSubject.cert_token);
     console.log(jsonSubject['full_name']);
+
+    var hasVal = "你好";
+    var noVal = {};
+    var testVal = noVal['val'] || hasVal;
+    console.log(encodeURI(testVal));
+
+    var num = 0x7FFFFFFF;
+    var buf = Buffer.alloc(4);
+    buf.writeInt32BE(num);
+    var hex = buf.toString('hex');
+    console.log(hex);
 })();
